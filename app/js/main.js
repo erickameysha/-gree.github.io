6bd1b94dbbdbf2dd531e45307e30ebd9b9e9b123
Eric <ekameyshaaa@gmail.com>
2021-08-20 15:25:50 +0300

    add page product-one

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,30 @@
+
+  
+  $('.product-slide__thumb').slick({
+     asNavFor: '.product-slide__big',
+      focusOnSelect: true,
+      slidesToShow: 4,
+        slidesToScroll: 1,
+        vertical: true,
+        draggable: false
+  });
+  $('.product-slide__big').slick({
+     asNavFor: '.product-slide__thumb',
+     draggable: false,
+     arrows: false,
+     fade: true
+  });
+
 $(function () {
+  $('.product-one__num').styler()
+  
+  $(".product-one__star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+
   $('.top-slider__inner').slick({
     arrows: false,
     dots: true
